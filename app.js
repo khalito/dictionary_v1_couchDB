03514dc59ab5af29ca17ec0c7ffdd52cb3e949ab
khalito <skscharaf@gmail.com>
2018-05-27 15:37:24 +0200

    Adds dropdown of words on the search page. Turns all DB requests for search result and list of words into Promises. Adds dropdown of words on the search page. Turns all DB requests for search results and the list of words into Promises.

--- app.js
+++ app.js
@@ -8,20 +8,26 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 
+function getAllArabicDocs(request, response) {
+    return new Promise(function (resolve, reject) {
+        Promise.promisifyAll(dict);
+
+        dict.listAsync({include_docs : true}).then(function(doc) {
+            let words = [];
+            doc.rows.forEach(function(row) {
+                let word = row.doc['word'];
+                if(word) {
+                    words.push(word)
+                }
+            })
+            resolve(words);
+        })
+    })
+}
+
 // List all words we have
 app.get('/all', function (request, response) {
-    Promise.promisifyAll(dict);
-
-    // Get a list of everything! Replace later with a view !
-    // See if we can use Arabic letters !!
-    dict.listAsync({include_docs : true}).then(function(doc) {
-        let words = [];
-        doc.rows.forEach(function(row) {
-            let word = row.doc['word'];
-            if(word) {
-                words.push(word)
-            }
-        })
+    getAllArabicDocs().then(function(words) {
         response.render('allWords', {
             'words' : words
         })
@@ -29,27 +35,39 @@ app.get('/all', function (request, response) {
 })
 
 // The search page
-// Add a drop down, populated by a view of all the words to make testing easier
 app.get('/search', function (request, response) {
-    response.render('index');
-})
+    getAllArabicDocs().then(function(words) {
+        response.render('search', {
+            'words' : words
+        })
+    })
+});
+
+
+
+
 
 // Replace the name page with a noun-specific page
-app.get('/search/name/result', function (request, response) {    
-    let search = '<p>You searched for: </p>' + request.query["name"] + '<br>';
-    let result = '<p>The result is: </p>';
-    let back = '<p><a href="http://localhost:3000/search">Back to search</a></p>';
-    let sorry = '<p>Sorry, this term is not in the database !</p>';
-    let query = request.query['name'];
+app.get('/search/result', function (request, response) {    
+    let query = request.query['searchedWord'];
 
     Promise.promisifyAll(dict);
-    dict.viewAsync('test', 'color', {'key' : query}).then(function(doc) {
-        response.send(search + result + doc.rows[0]['value'] + back);
+    dict.viewAsync('nouns', 'all', {'key' : query, include_docs : true}).then(function(doc) {
+        
+        let results = [];
+        //console.log(doc.rows[0]['value']); // <--------------- CONTINUE HERE AND GET THE VALUE !!!
+        doc.rows.forEach(function(row) {
+            results.push(row['value']);
+        })
+
+        response.render('result', {
+            'searchedWord' : query,
+            'results' : results 
+        });
+
     }).catch(TypeError, function(err) {
-        response.send(search + result + sorry + back);
         console.log("Type not found");
     }).catch(function(err){
-        response.send(search + result + sorry + back);
         console.log(err);
     });
 })
