ee86c1e8fd8d25a4fca3a48dc80f2031792e5e4b
khalito <skscharaf@gmail.com>
2018-06-01 00:11:47 +0200

    Displays word properties of nouns with basic styling.

--- app.js
+++ app.js
@@ -45,9 +45,12 @@ app.get('/search', function (request, response) {
 
 
 
-
-
-// Replace the name page with a noun-specific page
+// TO DO
+// - Add get request for a VERB VIEW ! Maybe using a "NEXT" function ? 
+// Or better, replace the NOUNS view with a ALL_WORDS view !!
+//
+// - Try using a word in Arabic letters !
+// 
 app.get('/search/result', function (request, response) {    
     let query = request.query['searchedWord'];
 
@@ -55,10 +58,32 @@ app.get('/search/result', function (request, response) {
     dict.viewAsync('nouns', 'all', {'key' : query, include_docs : true}).then(function(doc) {
         
         let results = [];
-        //console.log(doc.rows[0]['value']); // <--------------- CONTINUE HERE AND GET THE VALUE !!!
         doc.rows.forEach(function(row) {
-            results.push(row['value']);
+            let translation = row['value'];
+            let category = row['doc']['category'];
+            let root = row['doc']['root'];
+            let parent = row['doc']['parent'];
+            let parentLinkType = row['doc']['parentLinkType'];
+            if (category == 'noun') {
+                var nounDetails_quantity = row['doc']['nounDetails'][0]['quantity'];
+                var nounDetails_gender = row['doc']['nounDetails'][1]['gender'];
+            }
+            results.push({
+                translation : translation,
+                category : category,
+                root : root,
+                parent : parent,
+                parentLinkType : parentLinkType,
+                nounDetails_quantity : nounDetails_quantity,
+                nounDetails_gender : nounDetails_gender,
+            });
+            console.log('Noun data: \n')
+            console.log(doc); console.log('\n');
+            console.log(doc.rows); console.log('\n');
+            console.log(doc.rows[0]['doc']['nounDetails']);
+            console.log('Verb data: \n')
         })
+
 
         response.render('result', {
             'searchedWord' : query,
@@ -68,38 +93,6 @@ app.get('/search/result', function (request, response) {
     }).catch(TypeError, function(err) {
         console.log("Type not found");
     }).catch(function(err){
-        console.log(err);
-    });
-})
-
-// Replace the color page with a verb-specific page
-app.get('/search/color/resultEJS', function(request, response) {
-    let query = request.query['color'];
-    Promise.promisifyAll(dict);
-    
-    dict.viewAsync('test', 'name', {'key' : query}).then(function(doc){
-        response.render('result', {
-            'color' : doc.rows[0]['value']
-        });
-    })
-})
-
-// To be delted once the verb page is there.
-app.get('/search/color/result', function (request, response) {    
-    let search = '<p>You searched for: </p>' + request.query["color"] + '<br>';
-    let result = '<p>The result is: </p>';
-    let back = '<p><a href="http://localhost:3000/search">Back to search</a></p>';
-    let sorry = '<p>Sorry, this term is not in the database !</p>';
-    let query = request.query['color'];
-
-    Promise.promisifyAll(dict);
-    dict.viewAsync('test', 'name', {'key' : query}).then(function(doc) {
-        response.send(search + result + doc.rows[0]['value'] + back);
-    }).catch(TypeError, function(err) {
-        response.send(search + result + sorry + back);
-        console.log("Type not found");
-    }).catch(function(err){
-        response.send(search + result + sorry + back);
         console.log(err);
     });
 })
